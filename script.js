2086a19b91295e504f0befb76d784ff5d2b8985c
meravigliedelkenyaconpirlo <pirlowatamu5@gmail.com>
2026-05-22 07:59:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,54 @@ menu.addEventListener("click",()=>{
 nav.classList.toggle("active");
 
 });
+const slides=document.querySelectorAll(".slide");
+
+const dots=document.querySelectorAll(".dot");
+
+let current=0;
+
+
+function showSlide(index){
+
+slides.forEach(slide=>
+slide.classList.remove("active"));
+
+dots.forEach(dot=>
+dot.classList.remove("active"));
+
+slides[index].classList.add("active");
+
+dots[index].classList.add("active");
+
+}
+
+
+function nextSlide(){
+
+current++;
+
+if(current>=slides.length){
+
+current=0;
+
+}
+
+showSlide(current);
+
+}
+
+
+setInterval(nextSlide,5000);
+
+
+dots.forEach((dot,index)=>{
+
+dot.addEventListener("click",()=>{
+
+current=index;
+
+showSlide(current);
+
+});
+
+});
